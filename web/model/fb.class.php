decae82deac40b9eb67ba0cc53f953f736647168
Younes SADMI <younes.sadmi@gmail.com>
2016-01-19 19:02:07 +0100

    reparation de mon erreur sur mon merge

--- web/model/fb.class.php
+++ web/model/fb.class.php
@@ -4,6 +4,7 @@ class fb {
     private $registry;
     private $fb;
 
+
     public static function getInstance($arg) {
         if (!self::$instance instanceof self) {
             self::$instance = new self($arg);
@@ -19,40 +20,73 @@ class fb {
         trigger_error('Deserializing is not allowed.', E_USER_ERROR);
     }
 
-    private function __construct($registry) {
+    private function __construct($registry){
+        $permissions = ['email', 'user_likes'];
         require_once __SITE_PATH.'/includes/facebook-php-sdk-v4-5.0.0/src/Facebook/autoload.php';
-        $this->fb = new Facebook\Facebook([
+        $fb = new Facebook\Facebook([
             'app_id' => APP_ID,
             'app_secret' => APP_SECRET,
             'default_graph_version' => GRAPH_VERSION
         ]);
-        $this->fb->setDefaultAccessToken(APP_ACCESS_TOKEN);
-
-        if(!$this->is_user_connected()){
-            die('Not connected');
+        $helper = $fb->getRedirectLoginHelper();
+        if(empty($_SESSION['facebook_access_token'])){
+            $loginUrl = $helper->getLoginUrl(BASE_URL.'scripts/login.php', $permissions);
+            header('Location: '.$loginUrl);
+        }else{
+            if($this->tokenIsValid()){
+                $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);   
+            }else{
+                $loginUrl = $helper->getLoginUrl(BASE_URL.'scripts/login.php', $permissions);
+                header('Location: '.$loginUrl);
+            }
         }
-        if(!$this->is_app_allowed()){
-            die('Permission denied');
-        }
 
+        $this->fb = $fb;
+        $registry->is_admin = $this->is_admin($this->getCurrentUserId());
         $this->registry = $registry;
     }
 
-    private function is_user_connected(){
-        return true;
-        //on verra si on a besoin de checker la connexion ou pas
-    }
-    private function is_app_allowed(){
-        return true;
+    private function tokenIsValid(){
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://graph.facebook.com/me?access_token='.$_SESSION['facebook_access_token']);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $return = curl_exec($curl);
+        $return = json_decode($return, true);
+        curl_close($curl);
+        return !isset($return['error']);
     }
 
     public function get_roles(){
-        $response = $this->fb->get('/'.APP_ID.'/roles');
+        try{
+            $response = $this->fb->get('/'.APP_ID.'/roles', APP_ACCESS_TOKEN);
+        }catch(Facebook\Exceptions\FacebookResponseException $e){
+            echo $e->getMessage();
+            exit;
+        }
         return $response->getGraphEdge()->asArray();
+    }
+
+    public function getCurrentUserId(){
+        try{
+            $response = $this->fb->get('/me')->getGraphUser()->AsArray();
+        }catch(Facebook\Exceptions\FacebookResponseException $e){
+            echo $e->getMessage();
+            exit;
+        }
+        return $response['id'];
     }
 
     public function is_admin($id_user){
         $admins = $this->get_roles();
+        foreach($admins as $admin){
+            if($admin['role'] == 'administrators'){
+                if($admin['user'] == $id_user){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public function get_user_token(){
--- web/views/index.php
+++ web/views/index.php
@@ -1,1 +1,8 @@
-<h1>COMING SOON</h1>
+<pre><?php 
+
+
+var_dump($_SESSION);
+//var_dump($this->registry->fb->is_admin($this->registry->fb->getCurrentUserId()));
+
+
+?></pre>
