908154cf065be90f11ba2e7729be16cc6094bb7a
Younes SADMI <younes.sadmi@gmail.com>
2016-01-28 13:43:45 +0100

    404 page customed

--- web/views/not_found.php
+++ web/views/not_found.php
@@ -1,6 +1,29 @@
-<center>
-    <img src="<?php echo BASE_URL;?>img/error404.jpg" alt="Erreur d'affichage"/>
-    <br/>
-    <br/>
-    <br/>
-</center>
+<div>
+    <h1>404</h1>
+    <h2>Oh m****! Page non trouvée!</h2>
+</div>
+
+<style>
+    html {
+        background: url(https://38.media.tumblr.com/546c0cd48d71f210f9b67a389003790d/tumblr_neq0yw9rMA1tm16jjo1_500.gif) no-repeat center center fixed;
+        background-size: cover;
+    }
+    h1 {
+        font-size: 16em;
+        text-align:center;
+        color: rgba(255, 255, 255, 0.7);
+        margin-bottom: 0px;
+    }
+    h2 {
+        font-size: 1.7em;
+        margin: .2em .5em;
+        color: rgba(255, 255, 255, 0.6);
+    }
+    div {
+        position: absolute;
+        top: 30%;
+        margin-top: -8em;
+        width: 100%;
+        text-align: center;
+    }
+</style>
