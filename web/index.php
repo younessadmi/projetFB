40487ad5f78fdf6a01fa53be393f75b13d939739
Younes SADMI <younes.sadmi@gmail.com>
2016-02-09 23:45:52 +0100

    Compatibilité php 7 avec le typage strict

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <?php
+declare(strict_types=1);
 session_start();
-//declare(strict_types=1);
 error_reporting(E_ALL);
-ini_set("display_errors", 1);
+ini_set('display_errors', '1');
 
 $site_path = realpath(dirname(__FILE__));
 
