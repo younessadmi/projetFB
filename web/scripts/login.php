a1a37de15a6891f742cc8ed1106b2fdbd36ff863
Younes SADMI <younes.sadmi@gmail.com>
2016-02-11 21:07:39 +0100

    Affichage de l'erreur en cas d'annulation des permissions fb

--- web/scripts/login.php
+++ web/scripts/login.php
@@ -26,7 +26,6 @@ if(isset($accessToken)){
 
     // Now you can redirect to another page and use the
     // access token from $_SESSION['facebook_access_token']
-//    header('Location: '.((isset($_SERVER['HTTP_REFERER']))? $_SERVER['HTTP_REFERER'] : BASE_URL));
     header('Location: '. BASE_URL);
 }elseif ($error = $helper->getError()) {
     // The user denied the request
